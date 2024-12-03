7598e7061465aee5ca84fc52be30f0967e43214a
ana-znv <uzanzaev44@gmail.com>
2024-12-03 21:12:38 +0600

    created first operation icons

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,15 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.calculator.ui.theme.CalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,6 +48,62 @@ fun Main(modifier: Modifier = Modifier) {
         modifier = Modifier.fillMaxSize(),
         color = Color(0xFF2D2D2D)
     ) {
+        Row(
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier
+                .padding(bottom = 20.dp)
+        ) {
+            Button(
+                onClick = {},
+                shape = RoundedCornerShape(50),
+                modifier = Modifier
+                    .width(85.dp)
+                    .height(85.dp)
+            ) {
+                Text(
+                    text = "-",
+                    fontSize = 40.sp
+                )
+            }
+            Button(
+                onClick = {},
+                shape = RoundedCornerShape(50),
+                modifier = Modifier
+                    .width(85.dp)
+                    .height(85.dp)
 
+            ) {
+                Text(
+                    text = "÷",
+                    fontSize = 40.sp
+                )
+            }
+            Button(
+                onClick = {},
+                shape = RoundedCornerShape(50),
+                modifier = Modifier
+                    .width(85.dp)
+                    .height(85.dp)
+            ) {
+                Text(
+                    text = "×",
+                    fontSize = 40.sp
+                )
+            }
+            Button(
+                onClick = {},
+                shape = RoundedCornerShape(50),
+                modifier = Modifier
+                    .width(85.dp)
+                    .height(85.dp)
+            ) {
+                Text(
+                    text = "+",
+                    fontSize = 40.sp
+                )
+            }
+
+        }
     }
 }
